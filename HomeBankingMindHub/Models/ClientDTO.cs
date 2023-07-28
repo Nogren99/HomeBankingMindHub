bcb8868482633be1fff9bc0a832219163f7b9aa6
Juan Gabriel Rodriguez <nogren23@gmail.com>
2023-07-28 14:19:05 -0300

    DTOs

---
+++ HomeBankingMindHub/Models/ClientDTO.cs
@@ -0,0 +1,33 @@
+﻿using HomeBankingMindHub.Models;
+
+using System.Collections.Generic;
+
+using System.Text.Json.Serialization;
+
+
+
+namespace HomeBankingMindHub.dtos
+
+{
+
+    public class ClientDTO
+
+    {
+
+
+        //JsonIgnore, el cual cuando se renderize la respuesta de nuestro controlador NO devolverá visualmente el atributo Id.
+        [JsonIgnore]
+
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<AccountDTO> Accounts { get; set; }
+
+    }
+
+}
---
+++ HomeBankingMindHub/Models/TransactionDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HomeBankingMindHub.Models;
+
+using System.Collections.Generic;
+
+using System.Text.Json.Serialization;
+
+namespace HomeBankingMindHub.Models
+{
+    public class TransactionDTO
+    {
+        [JsonIgnore]
+
+        public long Id { get; set; }
+
+        public string Type { get; set; }
+        public double Amount { get; set; }
+
+        public String description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Account Account { get; set; }
+
+    }
+}
