14db958a6375efbd3e045a7430c0830c3160334c
Juan Gabriel Rodriguez <nogren23@gmail.com>
2023-07-29 13:49:04 -0300

    update

--- HomeBankingMindHub/Models/DbInitializer.cs
+++ HomeBankingMindHub/Models/DbInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace HomeBankingMindHub.Models
 {
@@ -20,6 +21,97 @@ namespace HomeBankingMindHub.Models
 
                 //guardamos
                 context.SaveChanges();
+            }
+            /*
+            else
+            {
+                var clients = new Client[]
+                {
+                    new Client { Email = "victor@gmail.com", FirstName="Victor", LastName="Coronado", Password="123123123"}
+                };
+
+                foreach (Client client in clients)
+                {
+                    context.Clients.Add(client);
+                }
+
+                //guardamos
+                context.SaveChanges();
+            }*/
+
+
+            if (!context.Account.Any())
+            {
+                var accountJuan = context.Clients.FirstOrDefault(c => c.Email == "juan@gmail.com");
+                if (accountJuan != null)
+                {
+                    var accounts = new Account[]
+                    {
+                        new Account {ClientId = accountJuan.Id, CreationDate = DateTime.Now, Number = string.Empty, Balance = 1000000 }
+                    };
+                    foreach (Account account in accounts)
+                    {
+                        context.Account.Add(account);
+                    }
+                    context.SaveChanges();
+
+                }
+            }
+            /*
+            else
+            {
+                var account1 = context.Clients.FirstOrDefault(c => c.Email == "victor@gmail.com");
+                if (account1 != null)
+                {
+                    var accounts = new Account[]
+                    {
+                        new Account {ClientId = account1.Id, CreationDate = DateTime.Now, Number = "VIN001", Balance = 10 }
+                    };
+                    foreach (Account account in accounts)
+                    {
+                        context.Account.Add(account);
+                    }
+                    context.SaveChanges();
+
+                }
+            }*/
+
+            if (!context.Transactions.Any())
+
+            {
+
+                var account1 = context.Account.FirstOrDefault(c => c.Number == "VIN001");
+
+                if (account1 != null)
+
+                {
+
+                    var transactions = new Transaction[]
+
+                    {
+
+                        new Transaction { AccountId= account1.Id, Amount = 10000, Date= DateTime.Now.AddHours(-5), Description = "Transferencia reccibida", Type = TransactionType.CREDIT.ToString() },
+
+                        new Transaction { AccountId= account1.Id, Amount = -2000, Date= DateTime.Now.AddHours(-6), Description = "Compra en tienda mercado libre", Type = TransactionType.DEBIT.ToString() },
+
+                        new Transaction { AccountId= account1.Id, Amount = -3000, Date= DateTime.Now.AddHours(-7), Description = "Compra en tienda xxxx", Type = TransactionType.DEBIT.ToString() },
+
+                    };
+
+                    foreach (Transaction transaction in transactions)
+
+                    {
+
+                        context.Transactions.Add(transaction);
+
+                    }
+
+                    context.SaveChanges();
+
+
+
+                }
+
             }
 
         }
---
+++ HomeBankingMindHub/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeBankingMindHub.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+
+        public string Type { get; set; }
+
+        public double Amount { get; set; }
+
+        public String Description{ get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Account Account { get; set; }
+
+        public long AccountId { get; set; }
+    }
+}
