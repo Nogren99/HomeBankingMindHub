06a56b1acac47deb52121362b0b2959c389c1ef5
Juan Gabriel Rodriguez <nogren23@gmail.com>
2023-07-31 23:48:02 -0300

    Loans con Front actualizado

--- HomeBankingMindHub/Models/Client.cs
+++ HomeBankingMindHub/Models/Client.cs
@@ -10,6 +10,7 @@ namespace HomeBankingMindHub.Models
         public string Email { get; set; }
         public string Password { get; set; }
         public ICollection<Account> Accounts { get; set; }
+        public ICollection<ClientLoan> ClientLoans { get; set; }
 
         //atajo: prop tira un snipet con nombre y sus setters y getters
     }
